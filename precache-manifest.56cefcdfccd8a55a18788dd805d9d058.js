87d0452033bd4706fca27571f3bd90fa458b9760
WPaczula <wojciech.paczula@gmail.com>
2020-03-26 22:24:26 +0000

    Updates

---
+++ precache-manifest.56cefcdfccd8a55a18788dd805d9d058.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb2b032720a4c19273f2273ad7f58c0",
+    "url": "/its-the-gorilla/index.html"
+  },
+  {
+    "revision": "99be6b7c6cbb87b7ac57",
+    "url": "/its-the-gorilla/static/css/main.cdefe495.chunk.css"
+  },
+  {
+    "revision": "c2ef059d2af3bdddfc34",
+    "url": "/its-the-gorilla/static/js/2.c03adb01.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/its-the-gorilla/static/js/2.c03adb01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99be6b7c6cbb87b7ac57",
+    "url": "/its-the-gorilla/static/js/main.d67ea34a.chunk.js"
+  },
+  {
+    "revision": "26d23db38a9c395be445",
+    "url": "/its-the-gorilla/static/js/runtime-main.bca46f0b.js"
+  },
+  {
+    "revision": "3d957a827d40cfd5bea6cbf5bcd1b27c",
+    "url": "/its-the-gorilla/static/media/background.3d957a82.jpg"
+  },
+  {
+    "revision": "8256ad05df049a512c23e70a9268386d",
+    "url": "/its-the-gorilla/static/media/pin.8256ad05.png"
+  },
+  {
+    "revision": "6597d9f01b86585efc72abf5698c4fd4",
+    "url": "/its-the-gorilla/static/media/wood.6597d9f0.jpg"
+  }
+]);
